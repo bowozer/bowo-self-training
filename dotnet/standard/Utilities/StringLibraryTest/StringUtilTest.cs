98e0ce3e3f57b9cf43e866b69f0cee83b2034294
Bowo <ardhio.sutjipto@outlook.com>
2019-10-08 16:06:13 +0800

    .Net Standard - Adding and running unit test methods

---
+++ dotnet/standard/Utilities/StringLibraryTest/StringUtilTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringLibrary;
+
+namespace StringLibraryTest
+{
+    [TestClass]
+    public class StringUtilTest
+    {
+        [TestMethod]
+        public void TestStartsWithUppercase()
+        {
+            // Arrange
+            string[] wordList = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+
+            foreach (string word in wordList)
+            {
+                // Act
+                bool result = word.StartsWithUpper();
+
+                // Assert
+                Assert.IsTrue(result, $"Expected for '{word}': true; Actual: {result}");
+            }
+        }
+
+        [TestMethod]
+        public void TestDoesNotStartWithUpper()
+        {
+            // Arrange
+            string[] wordList = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство", "1234", ".", ";", " " };
+
+            foreach (string word in wordList)
+            {
+                // Act
+                bool result = word.StartsWithUpper();
+
+                // Assert
+                Assert.IsFalse(result, $"Expected for '{word}': true; Actual: {result}");
+            }
+        }
+
+        [TestMethod]
+        public void TestDirectCallWithNullOrEmpty()
+        {
+            // Arrange
+            string[] wordList = { string.Empty, null };
+
+            foreach (string word in wordList)
+            {
+                // Act
+                bool result = StringUtil.StartsWithUpper(word);
+
+                // Assert
+                Assert.IsFalse(result, $"Expected for '{(word ?? "<null>")}': false; Actual: {result}");
+            }
+        }
+    }
+}
