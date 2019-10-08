287fe6eab0f27b1da9a383820842160fcffe9193
Bowo <ardhio.sutjipto@outlook.com>
2019-10-08 15:45:32 +0800

    .Net Standard - Creating .Net Standard Library

---
+++ dotnet/standard/Utilities/StringLibrary/StringUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringLibrary
+{
+    public static class StringUtil
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return false;
+
+            char firstChar = str[0];
+
+            return char.IsUpper(firstChar);
+        }
+    }
+}
