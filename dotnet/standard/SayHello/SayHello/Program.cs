9d59995ad316b3f6cd4939b0cc0c2b16e70727fc
Bowo <ardhio.sutjipto@outlook.com>
2019-10-08 09:34:56 +0800

    .Net Standard - A simple Hello World application

---
+++ dotnet/standard/SayHello/SayHello/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.Read();
+        }
+    }
+}
