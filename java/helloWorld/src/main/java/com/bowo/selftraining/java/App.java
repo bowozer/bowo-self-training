ebb89df8ab2d3217b646ee187d3fb82eca13d704
Bowo <ardhio.sutjipto@outlook.com>
2019-10-09 16:33:09 +0800

    Java - Hello World java app setup and developed by Visual Studio Code

---
+++ java/helloWorld/src/main/java/com/bowo/selftraining/java/App.java
@@ -0,0 +1,13 @@
+package com.bowo.selftraining.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Bowo!");
+        System.out.println(
+                "This is how you should execute me: java -cp \"F:\\codes\\cdc\\bowo-self-training\\java\\helloWorld\\target\\classes\\\" com.bowo.selftraining.java.App");
+    }
+}
